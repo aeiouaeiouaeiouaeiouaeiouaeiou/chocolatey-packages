fac2f651834f65d9ebdfe857678ee4a91f384ef2
Kyle Dixon <mrkylezd@gmail.com>
2019-04-14 15:25:50 -0400

    Updated to 1.8.0

--- manual/ppsspp/tools/chocolateyinstall.ps1
+++ manual/ppsspp/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://ppsspp.org/files/1_7_4/PPSSPPSetup.exe'
+$url = 'https://www.ppsspp.org/files/1_8_0/PPSSPPSetup.exe'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = $url
   softwareName   = 'ppsspp*'
-  checksum       = 'BF2F4D8DCCDD69FCC000339EBB2049D1A03672A50DF2CC7873AED836BB7F9C57'
+  checksum       = 'F2F935C59C7309B3CA42DF8414F6739AE3C3DAFF8F915AC08ADF0293CF5E63D9'
   checksumType   = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
