79b4554f170021ac66bf8d3593be744397fa957d
Kyle Dixon <mrkylezd@gmail.com>
2019-04-14 16:32:54 -0400

    Removed example packages. Added PPSSPP automatic package update script.

---
+++ automatic/ppsspp/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = ''
+$checksum = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'ppsspp*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/ppsspp/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'http://ppsspp.org/downloads.html'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    #http://ppsspp.org/files/{VERSION}/PPSSPPSetup.exe
+    $re  = "files/.+/PPSSPPSetup.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    $version = $version.replace('_', '.')
+    $url = 'http://ppsspp.org/' + $url
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update
