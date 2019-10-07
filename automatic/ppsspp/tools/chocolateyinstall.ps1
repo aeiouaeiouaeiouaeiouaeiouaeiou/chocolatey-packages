35c33df98ee0721885d4af37bcad0ba257e178c5
Chocolatey <chocolatey@realdimensions.net>
2019-10-07 00:06:26 +0000

    AU: 1 updated - ppsspp [skip ci] https://gist.github.com/1880bd22353df2d892459ee79f5761ad/893829bfce537ec8008623ad3b657415d6e5fd06

--- automatic/ppsspp/tools/chocolateyinstall.ps1
+++ automatic/ppsspp/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://ppsspp.org/files/1_9_0/PPSSPPSetup.exe'
-$checksum = '72C0EEE2302552F42C32089FA22E42B6EA28139572E69D8FD41EB90D6E09206C'
+$url = 'http://ppsspp.org/files/1_9_3/PPSSPPSetup.exe'
+$checksum = 'abd48ce599abca747d4408a6c7ce008958b3b01d6d910f8dd19f29d585c7bcfe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
