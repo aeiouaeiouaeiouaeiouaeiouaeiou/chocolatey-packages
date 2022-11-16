66e63fe217c4d9d22210a09f3d555ff3da880cf6
Paul <25939765+Cereal-Killa@users.noreply.github.com>
2022-11-16 19:33:24 +0000

    Added desktop shortcuts.

--- automatic/ppsspp/tools/chocolateyinstall.ps1
+++ automatic/ppsspp/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$desktopDir = [Environment]::GetFolderPath("Desktop")
+$programFiles = [Environment]::GetFolderPath("ProgramFiles")
 $url = 'http://ppsspp.org/files/1_13_2/PPSSPPSetup.exe'
 $checksum = '74f872f9692205312aeddbb675d3a3710270c9bed629ba6a590663ad9d56c271'
 
@@ -16,3 +18,5 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath ($desktopDir + '\PPSSPP (32-Bit).lnk') -TargetPath ($programFiles + '\PPSSPP\PPSSPPWindows.exe')
+Install-ChocolateyShortcut -ShortcutFilePath ($desktopDir + '\PPSSPP (64-Bit).lnk') -TargetPath ($programFiles + '\PPSSPP\PPSSPPWindows64.exe')
