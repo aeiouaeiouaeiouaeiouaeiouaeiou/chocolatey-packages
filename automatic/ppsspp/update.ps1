0bc6ef56ec3e785ddf5f171df4e9c542d95d6f2a
aeiouaeiouaeiouaeiouaeiouaeiou <aeioudev@outlook.com>
2023-09-26 11:18:27 +0300

    Test with WWW prefix

--- automatic/ppsspp/update.ps1
+++ automatic/ppsspp/update.ps1
@@ -14,12 +14,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    #http://ppsspp.org/files/{VERSION}/PPSSPPSetup.exe
+    #http://www.ppsspp.org/files/{VERSION}/PPSSPPSetup.exe
     $re  = "files/.+/PPSSPPSetup.exe"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
     $version = $url -split '/' | select -Last 1 -Skip 1
     $version = $version.replace('_', '.')
-    $url = 'https://ppsspp.org/' + $url
+    $url = 'https://www.ppsspp.org/' + $url
 
     $Latest = @{ URL32 = $url; Version = $version }
     return $Latest
