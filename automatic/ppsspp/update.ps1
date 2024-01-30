f64fbfadc451450bec6c789dac73147904c384f4
aeiouaeiouaeiouaeiouaeiouaeiou <aeioudev@outlook.com>
2024-01-31 00:46:21 +0300

    Minor cleanup

--- automatic/ppsspp/update.ps1
+++ automatic/ppsspp/update.ps1
@@ -5,7 +5,7 @@ $releases = 'https://www.ppsspp.org/download/'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
      }
@@ -13,13 +13,10 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    #http://www.ppsspp.org/files/{VERSION}/PPSSPPSetup.exe
-    $re  = "files/.+/PPSSPPSetup.exe"
+    $re = "files/.+/PPSSPPSetup.exe"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
     $version = $url -split '/' | select -Last 1 -Skip 1
     $version = $version.replace('_', '.')
-
     $Latest = @{ URL32 = $url; Version = $version }
     return $Latest
 }
